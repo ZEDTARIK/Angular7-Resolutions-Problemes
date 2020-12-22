12112de748062abc12ac0b2e4d3d0e58e88bd5f9
ettarak <ettarak.zouhair@gmail.com>
2020-12-22 12:01:36 +0100

    Coding, Routting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ForumOperationsComponent } from './components/forum-operations/forum-operations.component';
+import { ForumsComponent } from './components/forums/forums.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ForumDetailsComponent } from './components/forum-details/forum-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'formus', children: [
+      { path: '', component: ForumsComponent },
+      { path: 'operations', component: ForumOperationsComponent },
+      { path: 'details/:id', component: ForumDetailsComponent }
+    ]
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
